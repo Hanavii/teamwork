c2d21ad872c5c6bea74e969000d05d0de9774979
Liao Guojun <65930082+Hanavii@users.noreply.github.com>
2022-11-17 19:48:19 +0800

    Add files via upload

---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\微信下载\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-5gdhbyyv4c6c29ad --n quickstartFunctions --r --project "C:\Users\abc\WeChatProjects\miniprogram-3" --report_first --report
